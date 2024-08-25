7c8b92bcbd96ec16400455bd9202f8db213fe144
LandaMm <qwer.009771@gmail.com>
2024-08-25 22:24:53 +0200

    feat: project init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+</script>
+
+<div class="container">
+  <h1>Welcome to Tauri!</h1>
+</div>
+
+<style>
+  :root {
+    font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
+    font-size: 16px;
+    line-height: 24px;
+    font-weight: 400;
+
+    color: #0f0f0f;
+    background-color: #f6f6f6;
+
+    font-synthesis: none;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-text-size-adjust: 100%;
+  }
+
+  * {
+    padding: 0;
+    margin: 0;
+  }
+</style>
